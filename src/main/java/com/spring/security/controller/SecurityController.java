a20e9c8d75a9e224d586681a77656eb2330bcf91
sauravpandey <sauravpandeycs@gmail.com>
2023-10-05 17:34:31 +0530

    initial commit

---
+++ src/main/java/com/spring/security/controller/SecurityController.java
@@ -0,0 +1,15 @@
+package com.spring.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/security")
+public class SecurityController {
+    @GetMapping("/check")
+    public String test(){
+        System.out.println("HIiiiiiiiii");
+        return "It's working..";
+    }
+}
