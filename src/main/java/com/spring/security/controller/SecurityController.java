8c5c0a42e5c1eae18e5b42d8473cdeaefcee58e8
sauravpandey <sauravpandeycs@gmail.com>
2023-10-05 17:45:42 +0530

    replace print statement with loger

--- src/main/java/com/spring/security/controller/SecurityController.java
+++ src/main/java/com/spring/security/controller/SecurityController.java
@@ -1,5 +1,7 @@
 package com.spring.security.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/security")
 public class SecurityController {
+
+    private Logger log= LoggerFactory.getLogger(SecurityController.class);
     @GetMapping("/check")
     public String test(){
-        System.out.println("Hi");
+        log.info("check end point hits...");
         return "It's working..";
     }
 }
